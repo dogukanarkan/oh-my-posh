8c55113fd771e0855ba311628a3e4872b1d93202
Doğukan Arkan <dogukan@arkan.dev>
2025-01-30 02:08:20 +0300

    support multiple oci regions on the same account

--- src/segments/oci.go
+++ src/segments/oci.go
@@ -12,11 +12,12 @@ type Oci struct {
 	env   environment.Environment
 
 	Profile string
+	Account string
 	Region  string
 }
 
 func (a *Oci) Template() string {
-	return " {{ .Profile }}@{{ .Region }}"
+	return " {{ .Account }}@oracle/{{ .Region }}"
 }
 
 func (a *Oci) Init(props properties.Properties, env environment.Environment) {
@@ -26,9 +27,34 @@ func (a *Oci) Init(props properties.Properties, env environment.Environment) {
 
 func (a *Oci) Enabled() bool {
 	a.Profile = a.env.Getenv("OCI_CLI_PROFILE")
+	a.Account = a.getAccount()
 	a.Region = a.getRegion()
 
 	return a.Profile != ""
+}
+
+func (a *Oci) getAccount() string {
+	configPath := a.env.Getenv("OCI_CLI_CONFIG_FILE")
+	config := a.env.FileContent(configPath)
+	configLines := strings.Split(config, "\n")
+	configSection := fmt.Sprintf("[%s]", a.Profile)
+
+	var sectionActive bool
+	for _, line := range configLines {
+		if strings.HasPrefix(line, configSection) {
+			sectionActive = true
+			continue
+		}
+
+		if sectionActive && strings.HasPrefix(line, "account") {
+			splitted := strings.Split(line, "=")
+			if len(splitted) >= 2 {
+				return strings.TrimSpace(splitted[1])
+			}
+		}
+	}
+
+	return ""
 }
 
 func (a *Oci) getRegion() string {
